4895d9a59841987bee4e2a326b11b48bdbe5f06d
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2023-03-11 16:39:32 +0300

    line padding

--- Sources/SwiftUICharts/LineChart/LineView.swift
+++ Sources/SwiftUICharts/LineChart/LineView.swift
@@ -69,7 +69,7 @@ public struct LineView: View {
                              showBackground: true,
                              gradient: self.style.gradientColor
                         )
-                        .offset(x: 30, y: 0)
+                        .offset(x: 50, y: 0)
                         .onAppear(){
                             self.showLegend = true
                         }
@@ -81,15 +81,15 @@ public struct LineView: View {
                     .offset(x: 0, y: 10 )
                     MagnifierRect(currentNumber: self.$currentDataNumber, valueSpecifier: self.valueSpecifier)
                         .opacity(self.opacity)
-                        .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
+                        .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2 + 20, y: 36)
                 }
-                .frame(width: geometry.frame(in: .local).size.width, height: 240)
+                .frame(width: geometry.frame(in: .local).size.width, height: 210)
                 .gesture(DragGesture()
                 .onChanged({ value in
                     self.dragLocation = value.location
                     self.indicatorLocation = CGPoint(x: max(value.location.x - 55,0), y: 32)
                     self.opacity = 1
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
+                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width - 130, height: 150)
                     self.hideHorizontalLines = true
                 })
                     .onEnded({ value in
@@ -118,8 +118,7 @@ public struct LineView: View {
 struct LineView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LineView(data: ChartData(values: [("1", 10),("2", 30), ("3", 40)]), title: "Full chart", style: Styles.lineChartStyleOne)
-           
+            LineView(data: ChartData(values: [("2022", 10000),("2023", 15000) ]), title: "Full chart", style: Styles.lineChartStyleOne)
         }
     }
 }
